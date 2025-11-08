0dc9f8b3face708bb01841ddc12c6758a61b6f0c
Kridtin <kridtin.v@gmail.com>
2025-11-08 11:22:06 +0700

    ตกแต่งCss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,17 @@ import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
 export class AppComponent {
   title = 'Log-in';
   loginInput = {
-    username: 'userxten',
-    password: 'x10tech',
+    // username: 'userxten', //ทดสอบusername
+    // password: 'x10tech', //ทดสอบpassword
+    username: '',
+    password: '',
   };
   private getUrl = 'http://alpha-xten.com:8080/api/v1/test_login';
 
   private httpOp = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
-    }),
+    }), //ตั้งค่าheaderให้ตรงกับAPI
   };
 
   constructor(private http: HttpClient) {}
@@ -26,18 +28,27 @@ export class AppComponent {
       username: this.loginInput.username,
       password: this.loginInput.password,
     };
+    // if (!reqBody.username || !reqBody.password) {
+    //   alert('กรุณาตรวจสอบUsernameและPassword');
+    //   return;
+    // } //กรณีไม่ดักค่าจะทำให้เกิดRESPONSEที่3
     this.http.post<any>(this.getUrl, reqBody, this.httpOp).subscribe({
       next: (response) => {
         const responseData = response.meta?.response_data;
         const responseDesc = response.meta?.response_desc;
         const responseCode = response.meta?.response_code;
         const responseDatetime = response.meta?.response_datetime;
-        alert(
-          `${responseDatetime}:\n ${responseDesc}:\n ${responseCode}:\n ${responseData}`
-        );
+        if (!responseData) {
+          //RESPONSEที่3ไม่มีresponse_data
+          alert(`${responseDatetime}:\n ${responseDesc}:\n ${responseCode}`);
+        } //ในกรณีที่มีการตรวจสอบค่าusername,password
+        else
+          alert(
+            `${responseDatetime}:\n ${responseDesc}:\n ${responseCode}:\n ${responseData}`
+          );
       },
       error: (error) => {
-        const errorResponse = error.error?.meta?.response_data;
+        alert(`${error}`);
       },
     });
   }
